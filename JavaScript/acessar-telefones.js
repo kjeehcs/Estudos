aab94d8e5f36fa933f751d8822a9a302b114074c
Jeferson <kjeehcs@gmail.com>
2022-11-16 09:23:01 -0300

    Adicionando parentesco - JavaScript

--- JavaScript/acessar-telefones.js
+++ JavaScript/acessar-telefones.js
@@ -2,5 +2,7 @@ const cliente = {
     nome:"jeferson",
     idade:21,
     cpf:"12345",
-    email:"kjeehcs@gmail.com"
+    email:"kjeehcs@gmail.com",
+    fones: ["5591235498", "5521988743124"]
 }
+cliente.fones.forEach(fone => console.log(fone))
---
+++ JavaScript/compondo-objetos.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome:"jeferson",
+    idade:21,
+    cpf:"12345",
+    email:"kjeehcs@gmail.com"
+}
+cliente.dependentes = {
+    nome: "Paulina",
+    parentesco: "Cônjuge",
+    dataNasc: "18/05/2002"
+}
+
+cliente.dependentes.nome = "Paulina Santos";
+
+console.log(cliente);
